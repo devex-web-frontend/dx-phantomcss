005313224ec536758a89bd6ab62ce227a0d20391
agalakov <agalakov@devexperts.com>
2015-08-05 12:26:21 +0300

    initial

---
+++ cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var cp = require('child_process');
+var path = require('path');
+var glob = require('glob');
+
+var root = path.dirname(__filename);
+var cwd = process.cwd();
+
+var PHANTOMJS_EXECUTABLE = path.resolve('node_modules', 'casperjs', 'node_modules', '.bin', 'phantomjs');
+process.env['PHANTOMJS_EXECUTABLE'] = PHANTOMJS_EXECUTABLE;
+
+var CASPERJS_EXECUTABLE = path.resolve('node_modules', '.bin', 'casperjs');
+if (process.platform === 'win32') {
+	CASPERJS_EXECUTABLE += '.cmd';
+}
+
+var argv = process.argv.slice(2);
+if (argv.length === 0) {
+	throw new Error('Please specify some arguments!');
+}
+//iterate over argv and expand if glob
+argv = argv.map(function(arg) {
+	if (glob.hasMagic(arg)) {
+		return glob.sync(arg).join(' ');
+	} else {
+		return arg;
+	}
+});
+
+cp.execFileSync(CASPERJS_EXECUTABLE, argv, {
+	stdio: 'inherit'
+});
---
+++ index.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var system = require('system');
+var phantomcss = require('phantomcss');
+
+module.exports = {
+	resolveRelative: resolveRelative,
+	findRelative: findRelative,
+	captureSelector: captureSelector
+};
+
+/**
+ * Returns absolute path for relative file
+ * @param {String} relative
+ * @returns {String} absolute
+ */
+function resolveRelative(relative) {
+	var old = fs.workingDirectory;
+	var current = fs.absolute(system.args[4]); //do not change arguments order to casperjs!
+	console.log(system.args);
+	console.log('\n\n', current, '\n\n');
+	current = current.substr(0, current.lastIndexOf('/')); //do not use fs.separator
+	fs.changeWorkingDirectory(current);
+	var result = fs.absolute(relative);
+	fs.changeWorkingDirectory(old);
+	return result;
+}
+
+/**
+ * Acts similar to resolveRealtive, but checks if file exists and adds file:/// protocol
+ * @param {String} relative
+ * @returns {String} file
+ */
+function findRelative(relative) {
+	var result = resolveRelative(relative);
+	if (!fs.exists(result)) {
+		throw new Error('Path ' + result + ' does not exist!');
+	}
+	return 'file:///' + result;
+}
+
+/**
+ * Takes screenshot based on selector name and modifier
+ * @param {String} selector
+ * @param {String} [modifier]
+ */
+function captureSelector(selector, modifier) {
+	modifier = modifier ? ('_' + modifier) : '';
+	phantomcss.screenshot(selector, selector.replace(/\W/g, '_') + modifier);
+}
