10df602fa8062c5582b203dcee917ea7c0a626cc
agalakov <agalakov@devexperts.com>
2015-08-10 11:41:43 +0300

    override filenameGetter to include current test suite name

--- config/phantomcss.js
+++ config/phantomcss.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 
+var count = 0;
+
 //Be carefull setting absolute paths here because of dull replacement in onComplete callback
 var config = {
 	libraryRoot: './node_modules/phantomcss',
@@ -8,6 +10,19 @@ var config = {
 	comparisonResultRoot: './phantomcss/results',
 	prefixCount: true,
 	addLabelToFailedImage: false,
+	fileNameGetter: function fileNameGetter(root, filename) {
+		// globally override output filename
+		// files must exist under root
+		// and use the .diff convention
+		//override filename to contain current test suite name
+		var name = root + '/' + casper.test.currentSuite.name + '/' + count + '_' + filename;
+		count++;
+		if (fs.isFile(name + '.png')) {
+			return name + '.diff.png'; //this is from PhantomCSS!
+		} else {
+			return name + '.png';
+		}
+	},
 	onComplete: function onComplete(tests) {
 		tests.forEach(function(test) {
 			//this function is a pain in the ass... facepalm, PhantomCSS
